ab1a4eada866906aa6abd4229ba12b553a347d1f
Kurlyker <48961326+Kurlyker@users.noreply.github.com>
2021-12-15 14:00:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel, Field
 from typing import List
 
-DATABASE_URL = "postgresql://dbuser:dbpassword@127.0.0.1:5432/psqldbase"
+DATABASE_URL = "postgresql://kxcmhbkhvyyfel:bcea7a52633f6d2b8278c4880343774c2166835491d099bc28e3300e7d1ca5ba@ec2-52-213-119-221.eu-west-1.compute.amazonaws.com:5432/d7c6o4a367jfbo"
 database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
 
